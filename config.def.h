b8b66c507f6484d21fdc81f8ffc366ff85d95d5d
FR0ST1N <iamfrostin@gmail.com>
2021-04-24 10:39:19 +0530

    added gruvbox colors

--- config.def.h
+++ config.def.h
@@ -5,17 +5,15 @@ static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char *fonts[]          = { "JetBrains Mono NL:size=10" };
+static const char dmenufont[]       = "JetBrains Mono NL:size=10";
+static const char col_base00[]      = "#1d2021";
+static const char col_base01[]      = "#3c3836";
+static const char col_base05[]      = "#d5c4a1";
 static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	/*               fg         bg          border   */
+	[SchemeNorm] = { col_base05, col_base00, col_base01 },
+	[SchemeSel]  = { col_base05, col_base01, col_base05 },
 };
 
 /* tagging */
@@ -56,7 +54,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_base00, "-nf", col_base05, "-sb", col_base01, "-sf", col_base05, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 
 static Key keys[] = {
